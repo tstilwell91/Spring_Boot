9132da5980ef811f280d88a478f186cf4f7f638e
Terry <tstilwell@live.com>
2018-06-18 15:44:18 -0400

    Create UserDetailsRequestModel to convert JSON or other payload to Java object

--- mobile-app-ws/src/main/java/com/app/ws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/app/ws/ui/controller/UserController.java
@@ -4,8 +4,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.app.ws.ui.model.request.UserDetailsRequestModel;
 
 @RestController
 @RequestMapping("users") // http://localhost:8080/users
@@ -19,7 +22,7 @@ public class UserController {
 	}
 	
 	@PostMapping
-	public String createUser()
+	public String createUser(@RequestBody UserDetailsRequestModel userDetails)
 	{
 		return "createUser() was called";
 	}
